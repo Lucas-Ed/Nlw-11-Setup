97d8f16c7a89804e383940aff55730ac84d6470a
Lucas eduardo <lucas_tranpo@YAHOO.com.br>
2023-01-17 12:25:34 -0300

    Delete mobile1 directory

--- mobile1/src/components/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import { ActivityIndicator, View } from "react-native";
-
-export function Loading() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#09090A' }}>
-      <ActivityIndicator color="#7C3AED" />
-    </View>
-  )
-}
